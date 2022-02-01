de3b3394e78c6180052ed93cb982ed1a526dd6a2
Marcus Dunn <marcus.s.dunn@gmail.com>
2022-01-31 23:27:10 -0800

    added better docs.

--- backend/src/main/kotlin/TokenAuthenticator.kt
+++ backend/src/main/kotlin/TokenAuthenticator.kt
@@ -1,5 +1,4 @@
 import org.http4k.core.Filter
-import org.http4k.core.HttpHandler
 import org.http4k.core.Request
 import org.http4k.core.with
 import org.http4k.lens.RequestContextLens
@@ -8,7 +7,7 @@ import org.http4k.lens.RequestContextLens
  * Attaches a [TOKEN] to a request through [addAuthentication] if the request has enough information to create a valid token.
  * @param TOKEN the type of token. This should carry enough information to identify a user.
  * @param extractToken extracts the unverified string token from the request, returns null if the token cannot be found.
- * Defaults to extracting from an `Authentication: Bearer <TOKEN>` style header.
+ * Defaults to [extractBearerToken]
  * @param verifyToken verifies the token string, returning the token if verified, null if invalid.
  */
 class TokenAuthenticator<TOKEN>(
@@ -20,14 +19,14 @@ class TokenAuthenticator<TOKEN>(
      * @return a [Filter] which attaches the appropriate [TOKEN] to the request if the string token is verified.
      */
     fun addAuthentication(key: RequestContextLens<TOKEN?>): Filter {
-        return Filter { nextHandler: HttpHandler ->
-            contextHandler@{ request: Request ->
+        return Filter { nextHandler ->
+            response@{ request ->
                 val tokenString = extractToken(request)
-                    ?: return@contextHandler nextHandler(request)
+                    ?: return@response nextHandler(request)
                 val token = verifyToken(tokenString)
-                    ?: return@contextHandler nextHandler(request)
+                    ?: return@response nextHandler(request)
                 val requestWithContext = request.with(key of token)
-                nextHandler(requestWithContext)
+                return@response nextHandler(requestWithContext)
             }
         }
     }
@@ -35,6 +34,12 @@ class TokenAuthenticator<TOKEN>(
 
 private val bearerRegex = Regex("Bearer: (.*)")
 
+/**
+ * Extracts the `Token` from `Authentication: Bearer <Token>` style header.
+ * @param request the request containing an authentication bearer token.
+ * @return the string bearer token
+ * @see <a href="https://datatracker.ietf.org/doc/html/rfc6750#section-2.1">Authorization Request Header Field</a>
+ */
 fun extractBearerToken(request: Request): String? {
     val authenticationHeader = request.header("Authentication")
         ?: return null
