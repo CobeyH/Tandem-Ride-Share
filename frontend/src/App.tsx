5ffcd51561d8a8ec2c9a6ecc8328f2b29d38be07
Doug Johnson <doug@stormtree.ca>
2022-02-05 22:56:06 -0800

    Initialize firebase in react app.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+interface AppProps {
+  message: string;
+}
+
+function App({message}:AppProps) {
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +22,7 @@ function App() {
         >
           Learn React
         </a>
+        <p>{message}</p>
       </header>
     </div>
   );
