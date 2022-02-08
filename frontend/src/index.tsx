6cb66d040ee03ec66cc7413dea09786da753a409
Cobey Hollier <cobey.hollier@mac.com>
2022-02-08 01:14:20 -0800

    CH: Refactoring firebase, adding google auth

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,29 +5,6 @@ import { App } from "./App";
 import reportWebVitals from "./reportWebVitals";
 import * as serviceWorker from "./serviceWorker";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-//import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBrUBFWOzhP3XPuzi0ZYrNdNLcUWkLngTQ",
-  authDomain: "carpooling-6112a.firebaseapp.com",
-  projectId: "carpooling-6112a",
-  storageBucket: "carpooling-6112a.appspot.com",
-  messagingSenderId: "158541550551",
-  appId: "1:158541550551:web:927805d53ab973714ab192",
-  measurementId: "G-4QXLHB625R",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig, "web-frontend");
-//const analytics = getAnalytics(app);
-console.log("Firebase setup with name", app.name);
-
 serviceWorker.unregister();
 
 ReactDOM.render(
