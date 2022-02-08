4d728244d79ac386286fb472d4ee66f12404f9df
Cobey Hollier <cobey.hollier@mac.com>
2022-02-08 11:44:28 -0800

    CH: Adding react routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginForm from "./pages/LoginPage";
-import { ChakraProvider, Box, VStack, Grid, theme } from "@chakra-ui/react";
+import { ChakraProvider, Box, Grid, theme } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 
 export const App = () => (
@@ -9,9 +10,11 @@ export const App = () => (
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <LoginForm />
-        </VStack>
+        <Router>
+          <Routes>
+            <Route path="/login" element={<LoginForm />} />
+          </Routes>
+        </Router>
       </Grid>
     </Box>
   </ChakraProvider>
