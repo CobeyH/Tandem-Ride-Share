3b7e27eb3d9427581d0d674b39b2751e3648b80b
Cobey Hollier <cobey.hollier@mac.com>
2022-02-08 12:04:54 -0800

    CH: Adding routes and templates for other pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginForm from "./pages/LoginPage";
+import Groups from "./pages/Groups";
+import Rides from "./pages/Rides";
 import { ChakraProvider, Box, Grid, theme } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 
@@ -13,6 +15,8 @@ export const App = () => (
         <Router>
           <Routes>
             <Route path="/login" element={<LoginForm />} />
+            <Route path="/" element={<Groups />} />
+            <Route path="/rides" element={<Rides />} />
           </Routes>
         </Router>
       </Grid>
---
+++ frontend/src/pages/Groups.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { Flex, Box, Heading } from "@chakra-ui/react";
+
+export default function Groups() {
+  return (
+    <Flex width="full" align="start" justifyContent="center">
+      <Box textAlign="center">
+        <Heading>Groups Page</Heading>
+      </Box>
+    </Flex>
+  );
+}
