65d635598fbdeab583b88977436c31075af22070
Marcus Dunn <51931484+MarcusDunn@users.noreply.github.com>
2022-02-08 19:30:17 -0800

    Groups (#29)
    * Changed auth to use realtime-db
    
    * added groups and groups creation
    
    * removed _ in favor of ()
    
    * merge things
    
    Co-authored-by: Doug Johnson <doug@stormtree.ca>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,29 @@
 import * as React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import LoginForm from "./pages/LoginPage";
 import Groups from "./pages/Groups";
-import Rides from "./pages/Rides";
-import { ChakraProvider, Box, Grid, theme } from "@chakra-ui/react";
-import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import {Box, ChakraProvider, Grid, theme} from "@chakra-ui/react";
+import {ColorModeSwitcher} from "./ColorModeSwitcher";
 import Register from "./pages/Registration";
+import CreateGroup from "./pages/CreateGroup";
+import GroupPage from "./pages/GroupPage";
 
 export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box w="100%" h="200px" />
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <Router>
-          <Routes>
-            <Route path="/login" element={<LoginForm />} />
-            <Route path="/" element={<Groups groups= {[{name:"group1", number_members:3}, {name:"group2", number_members:6}]}/> } />
-            <Route path="/rides" element={<Rides />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
-        </Router>
-      </Grid>
-    </Box>
-  </ChakraProvider>
+    <ChakraProvider theme={theme}>
+        <Box w="100%" h="200px"/>
+        <Box textAlign="center" fontSize="xl">
+            <Grid minH="100vh" p={3}>
+                <ColorModeSwitcher justifySelf="flex-end"/>
+                <Router>
+                    <Routes>
+                        <Route path="/login" element={<LoginForm/>}/>
+                        <Route path="/" element={<Groups/>}/>
+                            <Route path="/group/new" element={<CreateGroup/>}/>
+                            <Route path="/group/:groupId" element={<GroupPage/>}/>
+                        <Route path="/register" element={<Register/>}/>
+                    </Routes>
+                </Router>
+            </Grid>
+        </Box>
+    </ChakraProvider>
 );
