08c658f8660781b45d7e04da91d305db3e36bcc3
Cobey Hollier <cobey.hollier@mac.com>
2022-02-09 20:37:26 -0800

    CH: Fixing formatting on remaining files

--- frontend/src/pages/Groups.tsx
+++ frontend/src/pages/Groups.tsx
@@ -1,35 +1,45 @@
 import * as React from "react";
-import {Button, Flex, Box, Heading, Link, Text, Spinner} from "@chakra-ui/react";
+import {
+  Button,
+  Flex,
+  Box,
+  Heading,
+  Link,
+  Text,
+  Spinner,
+} from "@chakra-ui/react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
-import {auth, db, logout} from "../firebase";
+import { auth, db, logout } from "../firebase";
 import { useEffect } from "react";
-import {ref} from "firebase/database";
-import {useListVals} from "react-firebase-hooks/database";
+import { ref } from "firebase/database";
+import { useListVals } from "react-firebase-hooks/database";
 
-export type Group = { id: string, name: string, rides: string[] };
+export type Group = { id: string; name: string; rides: string[] };
 
 export default function Groups() {
-    const [user, loading] = useAuthState(auth);
-    const [groups, loadingGroups, error] = useListVals<Group>(ref(db, "groups"));
+  const [user, loading] = useAuthState(auth);
+  const [groups, loadingGroups, error] = useListVals<Group>(ref(db, "groups"));
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        if (loading) return;
-        if (!user) return navigate("/login");
-    }, [user, loading]);
+  useEffect(() => {
+    if (loading) return;
+    if (!user) return navigate("/login");
+  }, [user, loading]);
 
-    return (
-        <Flex width="full" align="start" justifyContent="center">
-            <Box textAlign="center">
-                <Heading>Groups Page</Heading>
-                <Button onClick={logout}>Logout</Button>
-                {groups?.map((groups, i) => <Heading key={i}>{groups.name}</Heading>)}
-                {loadingGroups ? <Spinner/> : null}
-                {error ? <Text>{JSON.stringify(error)}</Text> : null}
-                <Link href={"group/new"}>Create a Group</Link>
-            </Box>
-        </Flex>
-    );
+  return (
+    <Flex width="full" align="start" justifyContent="center">
+      <Box textAlign="center">
+        <Heading>Groups Page</Heading>
+        <Button onClick={logout}>Logout</Button>
+        {groups?.map((groups, i) => (
+          <Heading key={i}>{groups.name}</Heading>
+        ))}
+        {loadingGroups ? <Spinner /> : null}
+        {error ? <Text>{JSON.stringify(error)}</Text> : null}
+        <Link href={"group/new"}>Create a Group</Link>
+      </Box>
+    </Flex>
+  );
 }
