7ddcb4d64cf93b5bcb00b373128492d98f91652a
Cobey Hollier <cobey.hollier@mac.com>
2022-02-09 19:55:17 -0800

    CH: Attempting to fix husky

--- frontend/src/pages/GroupPage.tsx
+++ frontend/src/pages/GroupPage.tsx
@@ -1,44 +1,45 @@
 import * as React from "react";
-import {Box, Flex, Heading, Spinner, Text} from "@chakra-ui/react";
-import {useNavigate, useParams} from "react-router-dom";
-import {db} from "../firebase";
-import {ref} from "firebase/database";
-import {Group} from "./Groups";
-import {Val} from "react-firebase-hooks/database/dist/database/types";
-import {useObjectVal} from "react-firebase-hooks/database";
+import { Box, Flex, Heading, Spinner, Text } from "@chakra-ui/react";
+import { useNavigate, useParams } from "react-router-dom";
+import { db } from "../firebase";
+import { ref } from "firebase/database";
+import { Group } from "./Groups";
+import { Val } from "react-firebase-hooks/database/dist/database/types";
+import { useObjectVal } from "react-firebase-hooks/database";
 
 export default function GroupPage() {
-    const navigate = useNavigate()
-    const groupId = useParams()["groupId"]
-    console.log(useParams())
-    if (groupId === undefined) {
-        console.log("figure something better to do here")
-        navigate("/")
-    }
-    const [group, loading, error] = useObjectVal<Group>(ref(db, `groups/${groupId}`))
+  const navigate = useNavigate();
+  const groupId = useParams()["groupId"];
+  console.log(useParams());
+  if (groupId === undefined) {
+    console.log("figure something better to do here");
+    navigate("/");
+  }
+  const [group, loading, error] = useObjectVal<Group>(
+    ref(db, `groups/${groupId}`)
+  );
 
-    return (
-        <Flex width="full" align="start" justifyContent="center">
-            <Box textAlign="center">
-                <Heading>Rides Page</Heading>
-            </Box>
+  return (
+    <Flex width="full" align="start" justifyContent="center">
+      <Box textAlign="center">
+        <Heading>Rides Page</Heading>
+      </Box>
 
-            {loading ?
-                <Spinner/> :
-                (error ?
-                        <Text>{JSON.stringify(error)}</Text> :
-                        (group ?
-                                <SingleGroup group={group}/> :
-                                <Text>No such group exists</Text>
-                        )
-                )
-            }
-
-        </Flex>
-    );
+      {loading ? (
+        <Spinner />
+      ) : error ? (
+        <Text>{JSON.stringify(error)}</Text>
+      ) : group ? (
+        <SingleGroup group={group} />
+      ) : (
+        <Text>No such group exists</Text>
+      )}
+    </Flex>
+  );
 }
 
-const SingleGroup = ({group}: { group: Val<Group> }) => <>
+const SingleGroup = ({ group }: { group: Val<Group> }) => (
+  <>
     <Heading>{group.name}</Heading>
-</>
-
+  </>
+);
