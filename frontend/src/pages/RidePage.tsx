7c4c81ad4479dabac949f1a0d502b5e12c754f69
Cobey Hollier <cobeyhollier@Cobeys-MacBook-Pro.local>
2022-02-09 20:06:51 -0800

    CH: Second test on mac

--- frontend/src/pages/RidePage.tsx
+++ frontend/src/pages/RidePage.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
+import React from "react";
 import { useParams } from "react-router-dom";
 import { MapContainer, Marker, TileLayer, useMap } from "react-leaflet";
 import { ref } from "firebase/database";
 import { useObjectVal } from "react-firebase-hooks/database";
 import { db } from "../firebase";
 import { Box, Heading } from "@chakra-ui/react";
-import { latLng, LatLng } from 'leaflet';
+import { latLng, LatLng } from "leaflet";
 
 export default function RidePage() {
   const { rideId } = useParams();
   const [ride] = useObjectVal<Ride>(ref(db, "rides/" + rideId));
-  let center = latLng([48.46557,-123.314736]);
+  let center = latLng([48.46557, -123.314736]);
   let startMarker, endMarker;
   if (ride) {
     center = findMidpoint(latLng(ride.start), latLng(ride.end));
-    startMarker = <Marker position={ride.start}/>;
-    endMarker = <Marker position={ride.end}/>;
+    startMarker = <Marker position={ride.start} />;
+    endMarker = <Marker position={ride.end} />;
   } else {
     startMarker = endMarker = null;
   }
@@ -23,21 +23,15 @@ export default function RidePage() {
   return (
     <Box>
       <Heading>{center.toString()}</Heading>
-    <MapContainer 
-      center={center} 
-      zoom={12}
-      scrollWheelZoom={false}
-      >
-      <ChangeView 
-        center={center} 
-        zoom={12} />
-      { startMarker }
-      { endMarker }
-      <TileLayer
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-    </MapContainer>
+      <MapContainer center={center} zoom={12} scrollWheelZoom={false}>
+        <ChangeView center={center} zoom={12} />
+        {startMarker}
+        {endMarker}
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+      </MapContainer>
     </Box>
   );
 }
@@ -56,9 +50,9 @@ export type Ride = {
   title: string;
   start: [number, number];
   end: [number, number];
-}
+};
 
 type MapView = {
   center: LatLng;
   zoom: number;
-}
+};
