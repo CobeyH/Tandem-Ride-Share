3c0e9aa14b103ae5139eae6c8e7a40572aef4bbe
Cobey Hollier <cobey.hollier@mac.com>
2022-02-09 19:41:32 -0800

    CH: Reformatting firebase file

--- frontend/src/firebase.tsx
+++ frontend/src/firebase.tsx
@@ -4,14 +4,7 @@ import {
   signInWithPopup,
   signOut,
 } from "firebase/auth";
-import {
-  getDatabase,
-  query,
-  ref,
-  set,
-  get,
-  equalTo,
-} from "firebase/database";
+import { getDatabase, query, ref, set, get, equalTo } from "firebase/database";
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
