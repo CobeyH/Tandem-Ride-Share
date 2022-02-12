cf891a8f91f19d4b9e04fcfd3b2569c0f27e3a49
Cobey Hollier <cobey.hollier@mac.com>
2022-02-11 22:12:43 -0800

    Header (#50)
    * CH: Adding header component
    
    * CH: Moving breadcrumbs out of header
    
    * CH: Conditionally render logout
    
    * CH: Removing breadcrumbs for now
    
    Co-authored-by: Cobey Hollier <cobeyhollier@w134-87-190-104.wireless.uvic.ca>
    Co-authored-by: Cobey Hollier <cobeyhollier@Cobeys-MacBook-Pro.local>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,17 +3,17 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginForm from "./pages/LoginPage";
 import GroupsListPage from "./pages/GroupsListPage";
 import { ChakraProvider, theme } from "@chakra-ui/react";
-import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import Register from "./pages/Registration";
 import CreateGroup from "./pages/CreateGroup";
 import GroupPage from "./pages/GroupPage";
 import RidePage from "./pages/RidePage";
 import CreateRide from "./pages/CreateRide";
 import JoinGroup from "./pages/JoinGroup";
+import Header from "./pages/Header";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <ColorModeSwitcher justifySelf="flex-end" />
+    <Header />
     <Router>
       <Routes>
         <Route path="/login" element={<LoginForm />} />
