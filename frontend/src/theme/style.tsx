99fe84a69cfec49914af9db658798d83d1c0f85b
Cobey Hollier <cobeyhollier@w134-87-187-005.wireless.uvic.ca>
2022-02-17 15:11:21 -0800

    Changing group list page name. Minor update

--- frontend/src/theme/style.tsx
+++ frontend/src/theme/style.tsx
@@ -1,7 +1,7 @@
 // theme.ts
 
 // 1. import `extendTheme` function
-import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
 import { mode } from "@chakra-ui/theme-tools";
 import { Dict } from "@chakra-ui/utils";
 import { lightTheme } from "./colours";
