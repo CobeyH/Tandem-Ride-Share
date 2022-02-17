a6b58eff7b53b1ee0b8c1c68fbeaa591dd6dc291
Cobey Hollier <cobey.hollier@mac.com>
2022-02-16 18:50:17 -0800

    CH: Adding user's name to header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,7 +5,9 @@ import {
   BreadcrumbItem,
   BreadcrumbLink,
   Spacer,
+  Box,
 } from "@chakra-ui/react";
+import { User } from "firebase/auth";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
@@ -17,6 +19,7 @@ export interface PageList {
 }
 
 const Header = (props: PageList) => {
+  const [user] = useAuthState(auth);
   return (
     <Flex
       width="100%"
@@ -31,7 +34,8 @@ const Header = (props: PageList) => {
       <Breadcrumbs pages={props.pages} />
       <Spacer />
       <ColorModeSwitcher justifySelf="flex-end" />
-      <LogoutButton />
+      <Box px={5}>{user?.displayName}</Box>
+      {user ? <LogoutButton /> : null}
     </Flex>
   );
 };
@@ -53,11 +57,7 @@ const Breadcrumbs = (props: PageList) => {
 };
 
 const LogoutButton = () => {
-  const [user] = useAuthState(auth);
-  if (user) {
-    return <Button onClick={logout}>Logout</Button>;
-  }
-  return null;
+  return <Button onClick={logout}>Logout</Button>;
 };
 
 export default Header;
