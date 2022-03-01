dc477815d2d94d7e13fba8af87292b6d6be0c400
Jae Park <jaepark052@gmail.com>
2022-03-01 00:48:30 -0800

    Made a new PR for ride time

--- frontend/src/components/RideCard.tsx
+++ frontend/src/components/RideCard.tsx
@@ -111,6 +111,7 @@ export default function RideCard({
                 ""
               )}
             </Flex>
+            <RideTimes startTime={ride.startDate} endTime={ride.endDate} />
             <AspectRatio ratio={16 / 10} mt="2">
               <MapView center={center} zoom={undefined} setMap={setMap}>
                 {startMarker}
@@ -226,6 +227,54 @@ function DriverDisplay({
       {displayDriverName ? (
         <Text ms={1} me={3}>{`${driverId ? driver : "Driver Needed"}`}</Text>
       ) : null}
+    </>
+  );
+}
+
+function RideTimes({
+  startTime,
+  endTime,
+}: {
+  startTime: string;
+  endTime: string;
+}) {
+  // make time strings pretty
+  const start_date = startTime.split("T")[0];
+  let start_time = startTime.split("T")[1];
+  const isPm_start = parseInt(start_time.split(":")[0]) >= 12;
+
+  start_time = isPm_start
+    ? `${parseInt(start_time.split(":")[0]) - 12}:${parseInt(
+        start_time.split(":")[1]
+      )}`
+    : start_time;
+
+  const end_date = endTime.split("T")[0];
+  let end_time = endTime.split("T")[1];
+  const isPm_end = parseInt(end_time.split(":")[0]) >= 12;
+
+  end_time = isPm_end
+    ? `${parseInt(end_time.split(":")[0]) - 12}:${parseInt(
+        end_time.split(":")[1]
+      )}`
+    : end_time;
+
+  return (
+    <>
+      <Flex flexDirection="row" m={2} align="center">
+        <Text>Start Date</Text>
+        <Spacer />
+        <Text>
+          {start_date} {start_time} {isPm_start ? "PM" : "AM"}
+        </Text>
+      </Flex>
+      <Flex flexDirection="row" m={2} align="center">
+        <Text>End Date</Text>
+        <Spacer />
+        <Text>
+          {end_date} {end_time} {isPm_end ? "PM" : "AM"}
+        </Text>
+      </Flex>
     </>
   );
 }
