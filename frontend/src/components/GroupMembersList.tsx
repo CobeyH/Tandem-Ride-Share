10e0b633655052d7fba90cdd921e805142cbe233
Cobey Hollier <cobey.hollier@mac.com>
2022-03-01 20:51:10 -0800

    CH: Hooking up real user querying

--- frontend/src/components/GroupMembersList.tsx
+++ frontend/src/components/GroupMembersList.tsx
@@ -7,16 +7,35 @@ import {
   DrawerCloseButton,
   DrawerHeader,
   DrawerBody,
-  Input,
   DrawerFooter,
+  Heading,
 } from "@chakra-ui/react";
+import { child, get, ref } from "firebase/database";
 import * as React from "react";
+import { useState } from "react";
+import { db, DB_USER_COLLECT } from "../firebase";
 
 const GroupMembersList = (props: {
   members: { [key: string]: boolean };
   ownerId: string;
 }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [groupMembers, setGroupMembers] = useState<string[]>();
+
+  const fetchUsers = () => {
+    const userRef = ref(db, DB_USER_COLLECT);
+    const queryPromises = Object.keys(props.members).map((userId) => {
+      return get(child(userRef, `${userId}`));
+    });
+    Promise.all(queryPromises).then((users) => {
+      const userNames = users.map((user) => user.val().name);
+      setGroupMembers(userNames);
+    });
+  };
+
+  React.useEffect(() => {
+    fetchUsers();
+  }, [isOpen]);
 
   return (
     <>
@@ -25,17 +44,19 @@ const GroupMembersList = (props: {
         <DrawerOverlay />
         <DrawerContent>
           <DrawerCloseButton />
-          <DrawerHeader>Create your account</DrawerHeader>
 
           <DrawerBody>
-            <Input placeholder="Type here..." />
+            {groupMembers?.map((userName, i) => (
+              <Heading size="md" key={i}>
+                {userName}
+              </Heading>
+            ))}
           </DrawerBody>
 
           <DrawerFooter>
             <Button variant="outline" mr={3} onClick={onClose}>
               Cancel
             </Button>
-            <Button colorScheme="blue">Save</Button>
           </DrawerFooter>
         </DrawerContent>
       </Drawer>
