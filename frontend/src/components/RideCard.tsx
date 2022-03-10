1b30d376fd5c556efbd990845720664b875fb3ab
Cobey Hollier <cobey.hollier@mac.com>
2022-03-09 18:10:19 -0800

    CH: Adding location based pickup point creation

--- frontend/src/components/RideCard.tsx
+++ frontend/src/components/RideCard.tsx
@@ -7,6 +7,7 @@ import {
   Flex,
   Heading,
   Icon,
+  IconButton,
   Spacer,
   Spinner,
   Switch,
@@ -42,6 +43,7 @@ import ChooseCar from "./ChooseCar";
 import GasCalculator from "./GasCalculator";
 import PickupMarkers from "./PickupMarkers";
 import { getOptimizedRoute, getReverseGeocode } from "../Directions";
+import { MdMyLocation } from "react-icons/all";
 
 export default function RideCard({
   rideId,
@@ -311,6 +313,24 @@ function PickupBar({ rideId, map }: { rideId: string; map: Map }) {
     };
   }, [addingPickup, ref, map]);
 
+  function getCurrentLocation() {
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        const latlng = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        } as LatLng;
+        console.log(latlng);
+        if (user) newPickupPoint(user.uid, rideId, latlng);
+        setAddingPickup(false);
+      },
+      (error) => {
+        alert("Failed to get user location");
+        console.log(error);
+      }
+    );
+  }
+
   async function newPickupPoint(
     userId: string,
     rideId: string,
@@ -364,7 +384,15 @@ function PickupBar({ rideId, map }: { rideId: string; map: Map }) {
           <Heading>{rideError}</Heading>
         ) : (
           <>
-            <Icon as={BsGeoAlt} w={6} h={6} />
+            {addingPickup ? (
+              <IconButton
+                aria-label="current-location"
+                icon={<MdMyLocation />}
+                onClick={getCurrentLocation}
+              ></IconButton>
+            ) : (
+              <Icon as={BsGeoAlt} w={6} h={6} />
+            )}
             <Text isTruncated>
               {addingPickup ? "Click on map to add point:" : text}
             </Text>
@@ -403,10 +431,6 @@ function RideTimesBar({ startTime }: { startTime: string }) {
         <Text>
           {start_date} {start_time} {isPm_start ? "PM" : "AM"}
         </Text>
-      </RideCardBar>
-      <RideCardBar>
-        <Text>End Date</Text>
-        <Spacer />
       </RideCardBar>
     </>
   );
