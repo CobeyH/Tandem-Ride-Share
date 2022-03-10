46c49434e8c798854573bcc5c6899f09a8ea3628
Cobey Hollier <cobey.hollier@mac.com>
2022-03-10 12:11:27 -0800

    CH: Setting start location from search

--- frontend/src/components/LocationSearch.tsx
+++ frontend/src/components/LocationSearch.tsx
@@ -1,12 +1,15 @@
-import { Input } from "@chakra-ui/react";
+import { Input, Menu, MenuItem, MenuList } from "@chakra-ui/react";
+import { LatLng } from "leaflet";
 import * as React from "react";
 import { useState } from "react";
+import { Location } from "../firebase/database";
+import { DEFAULT_CENTER } from "./MapView";
 
 const MQ_PREDICTION_URI = "http://www.mapquestapi.com/search/v3/prediction";
 const RESULT_LIMIT = 5;
 const COLLECTION = "address,poi";
 
-const LocationSearch = () => {
+const LocationSearch = (props: { setLatLng: (pos: LatLng) => void }) => {
   const [query, setQuery] = useState("");
   const [displayedLocs, setDisplayedLocs] = useState<Location[]>();
 
@@ -23,22 +26,43 @@ const LocationSearch = () => {
       `${MQ_PREDICTION_URI}?key=${process.env.REACT_APP_MQ_KEY}` +
         `&q=${query}` +
         `&limit=${RESULT_LIMIT}` +
-        `&collection=${COLLECTION}`
+        `&collection=${COLLECTION}` +
+        `&location=${DEFAULT_CENTER.lng},${DEFAULT_CENTER.lat}`
     )
       .then((res) => res.json())
       .catch((error) => console.log(error))
-      .then((result) => {
-        console.log(result);
-        setDisplayedLocs(result as Location[]);
+      .then((res) => {
+        console.log(res.results);
+        setDisplayedLocs(res.results as Location[]);
       });
   }
   return (
-    <Input
-      onChange={(e) => {
-        setQuery(e.currentTarget.value);
-        getLocations();
-      }}
-    ></Input>
+    <>
+      <Input
+        onChange={(e) => {
+          setQuery(e.currentTarget.value);
+          getLocations();
+        }}
+      />
+      <Menu isOpen={displayedLocs && displayedLocs.length > 0}>
+        <MenuList>
+          {displayedLocs?.map((l: Location) => {
+            return (
+              <MenuItem
+                onClick={() => {
+                  const position = l.place.geometry.coordinates;
+                  const latLng = new LatLng(position[1], position[0]);
+                  props.setLatLng(latLng);
+                }}
+                key={l.id}
+              >
+                {l.displayString}
+              </MenuItem>
+            );
+          })}
+        </MenuList>
+      </Menu>
+    </>
   );
 };
 
