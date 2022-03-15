8a225995ae7c885ca9004f16a4eafb481185bb42
Jae Park <jaepark052@gmail.com>
2022-03-15 14:50:43 -0700

    Full name is required when registration

--- frontend/src/pages/Registration.tsx
+++ frontend/src/pages/Registration.tsx
@@ -28,7 +28,10 @@ function Register() {
   const [name, setName] = useState("");
   const [user, loading] = useAuthState(auth);
   const register = () => {
-    if (!name) alert("Please enter name");
+    if (!name) {
+      alert("Please enter name");
+      return;
+    }
     registerWithEmailAndPassword(name, email, password);
   };
   const navigate = useNavigate();
