93882adfedb7a6259ece3c26fd063379f94bbebc
MarcusDunn <marcus.s.dunn@gmail.com>
2022-03-15 14:38:34 -0700

    added password resetting

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -18,7 +18,6 @@ import {
   ModalFooter,
   ModalBody,
   useToast,
-  Stack,
 } from "@chakra-ui/react";
 import {
   auth,
@@ -33,7 +32,6 @@ import { lightTheme } from "../theme/colours";
 import SignInRegister from "../components/SignInRegister";
 import PasswordField from "../components/PasswordField";
 import { MdMail } from "react-icons/all";
-import { confirmPasswordReset } from "firebase/auth";
 export default function Login() {
   const location = useLocation();
 
@@ -106,10 +104,6 @@ const ResetPasswordModal = ({
   email: string;
 }) => {
   const toast = useToast();
-  const [resetCode, setResetCode] = useState("");
-  const [emailSent, setEmailSent] = useState(false);
-  const [newPassword, setNewPassword] = useState("");
-  const [confirmNewPassword, setConfirmNewPassword] = useState("");
 
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
@@ -118,85 +112,35 @@ const ResetPasswordModal = ({
         <ModalHeader>Reset Password</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          {emailSent ? (
-            <Stack spacing={3}>
-              <Input
-                placeholder={"Reset code"}
-                type={"text"}
-                value={resetCode}
-                onChange={(e) => setResetCode(e.currentTarget.value)}
-              />
-              <Input
-                placeholder={"New password"}
-                type={"password"}
-                value={newPassword}
-                onChange={(e) => setNewPassword(e.currentTarget.value)}
-              />
-              <Input
-                placeholder={"Confirm New password"}
-                type={"password"}
-                value={confirmNewPassword}
-                onChange={(e) => setConfirmNewPassword(e.currentTarget.value)}
-              />
-              <Button
-                disabled={
-                  newPassword !== confirmNewPassword &&
-                  confirmNewPassword.length >= 6
-                }
-                onClick={() => {
-                  confirmPasswordReset(auth, resetCode, newPassword)
-                    .then(() => {
-                      toast({
-                        title: "Password Reset!",
-                        status: "success",
-                      });
-                      onClose();
-                    })
-                    .catch((err) => {
-                      toast({
-                        title: "Something Went Wrong",
-                        status: "error",
-                        description: err.code,
-                      });
-                    });
-                }}
-              >
-                Submit
-              </Button>
-            </Stack>
-          ) : (
-            <>
-              <InputGroup>
-                <Input
-                  type={"email"}
-                  value={email}
-                  onChange={(e) => setEmail(e.currentTarget.value)}
-                />
-                <InputLeftElement>
-                  <MdMail />
-                </InputLeftElement>
-              </InputGroup>
-              <Text p={4}>
-                We will send a password reset to your email, give it a couple
-                minutes to show up.
-              </Text>
-              <Button
-                p={4}
-                onClick={() =>
-                  sendPasswordReset(email).then(() => {
-                    toast({
-                      title: "Password Reset Email Sent!",
-                      status: "success",
-                      description: "It may take a second to show up.",
-                    });
-                    setEmailSent(true);
-                  })
-                }
-              >
-                Reset Password
-              </Button>
-            </>
-          )}
+          <InputGroup>
+            <Input
+              type={"email"}
+              value={email}
+              onChange={(e) => setEmail(e.currentTarget.value)}
+            />
+            <InputLeftElement>
+              <MdMail />
+            </InputLeftElement>
+          </InputGroup>
+          <Text p={4}>
+            We will send a password reset to your email, give it a couple
+            minutes to show up.
+          </Text>
+          <Button
+            p={4}
+            onClick={() =>
+              sendPasswordReset(email).then(() => {
+                toast({
+                  title: "Password Reset Email Sent!",
+                  status: "success",
+                  description: "It may take a second to show up.",
+                });
+                onClose();
+              })
+            }
+          >
+            Reset Password
+          </Button>
         </ModalBody>
       </ModalContent>
       <ModalFooter>
