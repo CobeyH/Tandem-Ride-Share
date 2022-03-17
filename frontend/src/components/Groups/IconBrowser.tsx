4d08634ba72eb0dd52b4c3e2c4b5bc194a56b8e5
Cobey Hollier <cobey.hollier@mac.com>
2022-03-16 22:46:00 -0700

    CH: Fixing searching filter bug

--- frontend/src/components/Groups/IconBrowser.tsx
+++ frontend/src/components/Groups/IconBrowser.tsx
@@ -21,6 +21,9 @@ const IconBrowser = (props: {
   const [startIndx, setStartIndx] = useState<number>(0);
   const [searchInput, setSearchInput] = useState("");
   const numPerPage = 25;
+  const filteredIcons = Object.keys(icons).filter((key: string) =>
+    key.toLowerCase().includes(searchInput.toLowerCase().replace(/\s+/g, ""))
+  );
   return (
     <VStack py={5} alignItems="center" spacing={3} w="100%">
       <InputGroup>
@@ -36,32 +39,23 @@ const IconBrowser = (props: {
       </InputGroup>
 
       <SimpleGrid columns={5} spacing={5} w="100%">
-        {Object.keys(icons)
-          .filter((key: string) =>
-            key
-              .toLowerCase()
-              .includes(searchInput.toLowerCase().replace(/\s+/g, ""))
-          )
-          .slice(startIndx, startIndx + 25)
-          .map((key: string) => {
-            return (
-              <IconButton
-                key={key}
-                aria-label={key}
-                w="100%"
-                h="100%"
-                bg={
-                  key == props.icon ? styleColors.mint : styleColors.lightBlue
-                }
-                as={(icons as { [k: string]: IconType })[key]}
-                isRound
-                p={1}
-                onClick={() => {
-                  props.updateIcon(key);
-                }}
-              />
-            );
-          })}
+        {filteredIcons.slice(startIndx, startIndx + 25).map((key: string) => {
+          return (
+            <IconButton
+              key={key}
+              aria-label={key}
+              w="100%"
+              h="100%"
+              bg={key == props.icon ? styleColors.mint : styleColors.lightBlue}
+              as={(icons as { [k: string]: IconType })[key]}
+              isRound
+              p={1}
+              onClick={() => {
+                props.updateIcon(key);
+              }}
+            />
+          );
+        })}
       </SimpleGrid>
       <HStack alignItems="center" pt={5}>
         <Button
@@ -71,7 +65,7 @@ const IconBrowser = (props: {
           Prev
         </Button>
         <Button
-          isDisabled={startIndx + numPerPage + 1 > Object.keys(icons).length}
+          isDisabled={startIndx + numPerPage + 1 > filteredIcons.length}
           onClick={() => setStartIndx(startIndx + numPerPage)}
         >
           Next
