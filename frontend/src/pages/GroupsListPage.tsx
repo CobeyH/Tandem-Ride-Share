d69775f49e07a6bb3b337b03e03b86773159571d
Cobey Hollier <cobey.hollier@mac.com>
2022-03-20 16:14:13 -0700

    CH: Fixing undefined error with icons

--- frontend/src/pages/GroupsListPage.tsx
+++ frontend/src/pages/GroupsListPage.tsx
@@ -81,7 +81,10 @@ export default function GroupsListPage() {
 const GroupListElement = (props: { group: Group; index: number }) => {
   const navigate = useNavigate();
   const photoName = props.group.profilePic;
-  const profileRef = photoName ? ref(storage, `${photoName}`) : undefined;
+  const profileRef =
+    photoName && photoName.startsWith("profilePics/")
+      ? ref(storage, `${photoName}`)
+      : undefined;
   const [profilePic, profilePicLoading] = useDownloadURL(profileRef);
 
   return (
