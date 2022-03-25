a81573b3626fd12b10792433c48bf248c0277600
Samantha Hill <samymhill@gmail.com>
2022-03-24 19:34:21 -0700

    Box size changed and duplicate person removed.

--- frontend/src/components/Promotional/Testimonial.tsx
+++ frontend/src/components/Promotional/Testimonial.tsx
@@ -81,9 +81,8 @@ export const Testimonal = () => (
             )}
           />
           <Text fontSize={{ base: "l", md: "xl" }} fontWeight="medium" mt="6">
-            &ldquo;Nice interface; seamless and logical all of the way. I like
-            that the fuel cost is calculated per person and shown on the app.
-            Dark mode is great.&rdquo;
+            &ldquo;This app reminded me how much money everyone from high school
+            to now owes me. 10/10&rdquo;
           </Text>
           <Quotee name="Oscar" jobTitle="Tandem User" mt="8" />
         </Flex>
@@ -111,31 +110,6 @@ export const Testimonal = () => (
             just arrange a ride out of the app&rdquo;
           </Text>
           <Quotee name="Adrian Ruigrok" jobTitle="Tandem User" mt="8" />
-        </Flex>
-      </Box>
-      <Box
-        maxW="3xl"
-        mx="auto"
-        px={{ base: "6", md: "8" }}
-        pt="12"
-        pb="16"
-        borderColor={styleColors.deepBlue}
-        borderWidth={0}
-        borderRadius={15}
-        bgColor="rgba(256,256,256,0.5)"
-      >
-        <Flex direction="column" align="center" textAlign="center">
-          <ImQuotesLeft
-            color={useColorModeValue(
-              "${styleColors.deepBlue}",
-              "${styleColors.deepBlue}"
-            )}
-          />
-          <Text fontSize={{ base: "l", md: "xl" }} fontWeight="medium" mt="6">
-            &ldquo;This app reminded me how much money everyone from high school
-            to now owes me. 10/10&rdquo;
-          </Text>
-          <Quotee name="Oscar" jobTitle="Satisfied User" />
         </Flex>
       </Box>
     </SimpleGrid>
