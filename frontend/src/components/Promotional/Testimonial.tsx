5007c86466e7cd7e30ca2d1ab563d1d8e43f6191
Samantha Hill <samymhill@gmail.com>
2022-03-24 19:13:22 -0700

    git issues fixed... sorry -sam

--- frontend/src/components/Promotional/Testimonial.tsx
+++ frontend/src/components/Promotional/Testimonial.tsx
@@ -114,30 +114,30 @@ export const Testimonal = () => (
         </Flex>
       </Box>
       <Box
-      maxW="3xl"
-      mx="auto"
-      px={{ base: "6", md: "8" }}
-      pt="12"
-      pb="16"
-      borderColor={styleColors.deepBlue}
-      borderWidth={0}
-      borderRadius={15}
-      bgColor="rgba(256,256,256,0.5)"
-    >
-      <Flex direction="column" align="center" textAlign="center">
-        <ImQuotesLeft
-          color={useColorModeValue(
-            "${styleColors.deepBlue}",
-            "${styleColors.deepBlue}"
-          )}
-        />
-        <Text fontSize={{ base: "l", md: "xl" }} fontWeight="medium" mt="6">
-          &ldquo;This app reminded me how much money everyone from high school
-          to now owes me. 10/10&rdquo;
-        </Text>
-        <Quotee name="Oscar" jobTitle="Satisfied User" />
-      </Flex>
-    </Box>
+        maxW="3xl"
+        mx="auto"
+        px={{ base: "6", md: "8" }}
+        pt="12"
+        pb="16"
+        borderColor={styleColors.deepBlue}
+        borderWidth={0}
+        borderRadius={15}
+        bgColor="rgba(256,256,256,0.5)"
+      >
+        <Flex direction="column" align="center" textAlign="center">
+          <ImQuotesLeft
+            color={useColorModeValue(
+              "${styleColors.deepBlue}",
+              "${styleColors.deepBlue}"
+            )}
+          />
+          <Text fontSize={{ base: "l", md: "xl" }} fontWeight="medium" mt="6">
+            &ldquo;This app reminded me how much money everyone from high school
+            to now owes me. 10/10&rdquo;
+          </Text>
+          <Quotee name="Oscar" jobTitle="Satisfied User" />
+        </Flex>
+      </Box>
     </SimpleGrid>
   </Box>
 );
