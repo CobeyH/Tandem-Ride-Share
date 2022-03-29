ccb8870c57ff40f1335b608022f5e13c1dbdb7c6
Jae Park <jaepark052@gmail.com>
2022-03-28 22:53:29 -0700

    Added report a bug button

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -13,15 +13,22 @@ import {
   MenuList,
   ModalCloseButton,
   IconButton,
+  Tooltip,
+  ModalOverlay,
+  Text,
+  ModalFooter,
+  useDisclosure,
+  Link,
+  useToast,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import { logout, auth } from "../firebase/firebase";
-import { FaHome, MdEmail } from "react-icons/all";
+import { FaHome, MdEmail, FaBug } from "react-icons/all";
 import { User } from "firebase/auth";
 import AddCar from "./Profiles/AddCar";
-import { lightTheme } from "../theme/colours";
+import { lightTheme, styleColors } from "../theme/colours";
 import ManageCars from "./Profiles/ManageCars";
 import { useNavigate } from "react-router-dom";
 
@@ -48,6 +55,7 @@ const Header = ({ isNested }: { isNested?: boolean }) => {
       <Spacer />
       {user ? (
         <Menu>
+          <ReportBug />
           <MenuButton as={Button}>Profile</MenuButton>
           <MenuList zIndex={3}>
             <Settings user={user} />
@@ -91,6 +99,69 @@ const Settings = (props: { user: User }) => {
         </ModalContent>
       </Modal>
     </MenuItem>
+  );
+};
+
+const ReportBug = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const toast = useToast();
+
+  return (
+    <>
+      <Tooltip hasArrow label="Found a bug?" bg="gray.300" color="black">
+        <IconButton
+          mr="2"
+          variant="ghost"
+          aria-label="report a bug"
+          icon={<FaBug />}
+          onClick={onOpen}
+        />
+      </Tooltip>
+
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Report a Bug</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>
+              Did you find a bug? Please report to us so we can make your
+              experience better!
+            </Text>
+            <br />
+            <Text>
+              You can reach out to our support team via{" "}
+              <Link
+                color={styleColors.mainBlue}
+                onClick={() => {
+                  navigator.clipboard
+                    .writeText("CobeyHollier@gmail.com")
+                    .then(() => {
+                      toast({
+                        title: "Copied Email to Clipboard",
+                        status: "success",
+                        isClosable: true,
+                      });
+                    });
+                }}
+              >
+                email
+              </Link>{" "}
+            </Text>
+            <br />
+            <Text>
+              Please provide as much context as possible. For example, the page
+              you were in, the function you were using, etc
+            </Text>
+          </ModalBody>
+          <ModalFooter>
+            <Button mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
