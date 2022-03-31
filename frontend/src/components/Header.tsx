f6cf128da1975798800d8d3102f2d5545eeb4f03
Cobey Hollier <cobeyhollier@Cobeys-MacBook-Pro.local>
2022-03-31 09:34:23 -0700

    CH: Fixing home buttons redirect location

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -57,7 +57,7 @@ const Header = ({
       <LogoName />
       {isNested ? (
         <IconButton
-          onClick={() => navigate("/")}
+          onClick={() => navigate("/welcome")}
           aria-label="home"
           icon={<FaHome />}
           mx={3}
