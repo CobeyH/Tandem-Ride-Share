2a8b98fc34f41ffe20aa3d9b2ad329348c9dc629
Cobey Hollier <cobey.hollier@mac.com>
2022-04-17 15:01:39 -0700

    CH: Adding another login test

--- frontend/cypress/integration/login.spec.js
+++ frontend/cypress/integration/login.spec.js
@@ -1,26 +1,28 @@
-import "../support/commands"
-// login.spec.js created with Cypress
-//
-// Start writing your Cypress tests below!
-// If you're unfamiliar with how Cypress works,
-// check out the link below and learn how to write your first test:
-// https://on.cypress.io/writing-first-test
+beforeEach(() => {
+  cy.visit("/login");
+  cy.logout(); // This command doesn't show up in the tests
+});
 
-describe("Login", () => {
+describe("Login Using UI", () => {
   it("is accessible via url", () => {
-    cy.visit("/login");
     cy.contains("Sign in");
+  });
+  it("existing user can log in", () => {
+    cy.get("[data-cy=email]").type("example@example.com");
+    cy.get("[data-cy=password]").type("password");
+    cy.get("[data-cy=auth-submit]").click();
+    cy.url().should("include", "/welcome");
   });
 });
 
-describe("Authentication", () => {
+describe("Login without UI", () => {
   before(() => {
     //TODO: This is hard coded. Needs to be changed
     cy.login("ftVmZ5dG5cZVUQjbxW1w1198jcTk");
-  })
-  
+  });
+
   it("Visit a page that requires authentication", () => {
     cy.visit("/welcome");
-    cy.contains("Welcome to Tandem!")
-  })
-})
+    cy.contains("Welcome to Tandem!");
+  });
+});
