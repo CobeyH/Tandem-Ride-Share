49d830d5bbf8855a56ae416176da7c7e927a90c1
Cobey Hollier <cobey.hollier@mac.com>
2022-04-19 18:00:59 -0700

    CH: Add car test

---
+++ frontend/cypress/integration/addCar.spec.js
@@ -0,0 +1,18 @@
+before(() => {
+  cy.visit("/welcome");
+});
+
+describe("Add car modal", () => {
+  it("is accessible from menu", () => {
+    cy.get("[data-cy=main-menu]").click();
+    cy.get("[data-cy=add-car]").click();
+  });
+  // The user can add a car and the success toast notification is displayed
+  it("add car with correct data entry", () => {
+    cy.get("[data-cy=car-name]").type("Test-Car");
+    cy.get("[data-cy=next-step]").click();
+    cy.get("[data-cy=next-step]").last().click();
+    cy.get("[data-cy=next-step]").last().click();
+    cy.get(".chakra-alert").contains("Car created");
+  });
+});
