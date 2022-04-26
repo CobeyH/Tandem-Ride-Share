06a7ccd118f550bbb1053aea7acc3f24a5128954
Cobey Hollier <cobey.hollier@mac.com>
2022-04-25 18:19:00 -0700

    CH: Trying to fix broken test

--- frontend/cypress/integration/tutorials.spec.js
+++ frontend/cypress/integration/tutorials.spec.js
@@ -7,11 +7,6 @@ function completeTutorial(length) {
   cy.contains("Last").click();
 }
 
-beforeEach(() => {
-  cy.logout();
-  cy.login();
-});
-
 // If an element referenced by a tutorial is removed, then the tutorial will skip a step.
 // We should ensure that tutorials always have the correct number of steps.
 describe("Tutorials complete from start to finish", () => {
