d00ac39450a5446f1fa124628f4ec2a8f7d75a18
Cobey Hollier <cobey.hollier@mac.com>
2022-04-25 18:32:39 -0700

    CH: Proper reset between tests

--- frontend/cypress/integration/addCar.spec.js
+++ frontend/cypress/integration/addCar.spec.js
@@ -1,5 +1,6 @@
 describe("Add car modal", () => {
-  before(() => {
+  beforeEach(() => {
+    cy.logout();
     cy.login();
     cy.visit("/welcome");
   });
@@ -9,6 +10,8 @@ describe("Add car modal", () => {
   });
   // The user can add a car and the success toast notification is displayed
   it("add car with correct data entry", () => {
+    cy.get("[data-cy=main-menu]").click();
+    cy.get("[data-cy=add-car]").click();
     cy.get("[data-cy=car-name]").type("Test-Car");
     cy.get("[data-cy=next-step]").click();
     cy.get("[data-cy=next-step]").last().click();
