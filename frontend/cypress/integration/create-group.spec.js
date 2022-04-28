ee512acfcc39295a8decf3ad38ee4202e734ed83
Cobey Hollier <cobey.hollier@mac.com>
2022-04-27 21:32:04 -0700

    CH: Create group with icon test

--- frontend/cypress/integration/create-group.spec.js
+++ frontend/cypress/integration/create-group.spec.js
@@ -1,7 +1,7 @@
 describe("Group Creation", () => {
   beforeEach(() => {
     cy.logout();
-    cy.login();
+    cy.login("HoZFgyVFlgv1WhrhJxBgzuBkgH79");
   });
 
   it("Create group button is accessible", () => {
@@ -26,7 +26,7 @@ describe("Group Creation", () => {
     cy.get("[data-cy=group-initials]").should("have.length", 2);
   });
 
-  it.only("Create group with media", () => {
+  it("Create group with media", () => {
     cy.visit("/welcome");
     cy.get("[data-cy=new-group]").click();
     cy.get("[data-cy=group-name]").type("Cypress Testing Media");
@@ -53,5 +53,25 @@ describe("Group Creation", () => {
     cy.get("[data-cy=next-step]").last().click();
     cy.get("[data-cy=group-banner]");
     cy.get("[data-cy=group-photo]");
+  });
+
+  it("Create group with icon", () => {
+    cy.visit("/welcome");
+    cy.get("[data-cy=new-group]").click();
+    cy.get("[data-cy=group-name]").type("Cypress Testing Group");
+    cy.get("[data-cy=next-step]").click();
+    // Don't enter description
+    cy.get("[data-cy=next-step]").last().click();
+    // Use default plan
+    cy.get("[data-cy=next-step]").last().click();
+    // Use default publicity
+    cy.get("[data-cy=next-step]").last().click();
+    // Upload icon
+    cy.get("[data-cy=icon-browser]").click();
+    cy.get("[data-cy=icon-search-filter]").type("Bike");
+    cy.get("[data-cy=icon-option]").first().click();
+    // Submit Group
+    cy.get("[data-cy=next-step]").last().click();
+    cy.get("[data-cy=group-icon]").should("have.length", 2);
   });
 });
