6ffa47c8414c2c45fa422dded9b1c659c24bec41
Cobey Hollier <cobey.hollier@mac.com>
2022-06-10 18:38:26 -0700

    CH: Adding capacitor and building IOS/Android apps

---
+++ frontend/android/app/src/main/java/com/tandem/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.tandem.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tandem.app',
+  appName: 'Tandem',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
