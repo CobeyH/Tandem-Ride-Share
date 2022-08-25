423cdf3749154279426b2f85bf9f4885ec00cae2
Cobey Hollier <cobey.hollier@mac.com>
2022-08-24 19:05:58 -0700

    CH: Fixing spacing issue in testimonials

--- frontend/src/components/Promotional/Testimonial.tsx
+++ frontend/src/components/Promotional/Testimonial.tsx
@@ -4,6 +4,7 @@ import {
   Text,
   useColorModeValue,
   SimpleGrid,
+  Center,
 } from "@chakra-ui/react";
 import * as React from "react";
 import { ImQuotesLeft } from "react-icons/all";
@@ -12,107 +13,129 @@ import { Quotee } from "./Quotee";
 
 export const Testimonal = () => (
   <Box as="section">
-    <SimpleGrid columns={{ base: 1, sm: 1, md: 2 }} spacing={5}>
-      <Box
-        maxW="3xl"
-        px={{ base: "6", md: "8" }}
-        pt="12"
-        pb="16"
-        borderWidth={0}
-        borderRadius={15}
-        bgColor={useColorModeValue("rgba(256,256,256,0.5)", "rgba(0,0,0,0.5)")}
-      >
-        <Flex direction="column" align="center" textAlign="center">
-          <ImQuotesLeft
-            color={useColorModeValue(
-              "${styleColors.deepBlue}",
-              "${styleColors.deepBlue}"
-            )}
-          />
-          <Box mt={42}>
+    <Center>
+      <SimpleGrid columns={{ base: 1, sm: 1, md: 2 }} spacing={5} maxW="6xl">
+        <Box
+          px={{ base: "6", md: "8" }}
+          pt="12"
+          pb="16"
+          borderWidth={0}
+          borderRadius={15}
+          bgColor={useColorModeValue(
+            "rgba(256,256,256,0.5)",
+            "rgba(0,0,0,0.5)"
+          )}
+        >
+          <Flex direction="column" align="center" textAlign="center">
+            <ImQuotesLeft
+              color={useColorModeValue(
+                "${styleColors.deepBlue}",
+                "${styleColors.deepBlue}"
+              )}
+            />
+            <Box mt={42}>
+              <Text
+                fontSize={{ base: "l", md: "xl" }}
+                fontWeight="medium"
+                mt="6"
+              >
+                &ldquo;Clear layout & fun to use!&rdquo;
+              </Text>
+              <Quotee name="Tim Ruigrok" jobTitle="Tandem User" mt="8" />
+            </Box>
+          </Flex>
+        </Box>
+        <Box
+          px={{ base: "6", md: "8" }}
+          pt="12"
+          pb="16"
+          borderColor={styleColors.deepBlue}
+          borderWidth={0}
+          borderRadius={15}
+          bgColor={useColorModeValue(
+            "rgba(256,256,256,0.5)",
+            "rgba(0,0,0,0.5)"
+          )}
+        >
+          <Flex direction="column" align="center" textAlign="center">
+            <ImQuotesLeft
+              color={useColorModeValue(
+                "${styleColors.deepBlue}",
+                "${styleColors.deepBlue}"
+              )}
+            />
+            <Box mt={41}>
+              <Text
+                fontSize={{ base: "l", md: "xl" }}
+                fontWeight="medium"
+                mt="6"
+              >
+                &ldquo;Fun and fresh perspective to look at car pooling!&rdquo;
+              </Text>
+              <Quotee name="Kutay" jobTitle="Tandem User" mt="8" />
+            </Box>
+          </Flex>
+        </Box>
+        <Box
+          px={{ base: "6", md: "8" }}
+          pt="12"
+          pb="16"
+          borderColor={styleColors.deepBlue}
+          borderWidth={0}
+          borderRadius={15}
+          bgColor={useColorModeValue(
+            "rgba(256,256,256,0.5)",
+            "rgba(0,0,0,0.5)"
+          )}
+        >
+          <Flex direction="column" align="center" textAlign="center">
+            <ImQuotesLeft
+              color={useColorModeValue(
+                "${styleColors.deepBlue}",
+                "${styleColors.deepBlue}"
+              )}
+            />
+            <Box mt={47}>
+              <Text
+                fontSize={{ base: "l", md: "xl" }}
+                fontWeight="medium"
+                mt="6"
+              >
+                &ldquo;This app reminded me how much money everyone from high
+                school to now owes me. 10/10&rdquo;
+              </Text>
+            </Box>
+            <Quotee name="Oscar" jobTitle="Tandem User" mt="8" />
+          </Flex>
+        </Box>
+        <Box
+          px={{ base: "6", md: "8" }}
+          pt="12"
+          pb="16"
+          borderColor={styleColors.deepBlue}
+          borderWidth={0}
+          borderRadius={15}
+          bgColor={useColorModeValue(
+            "rgba(256,256,256,0.5)",
+            "rgba(0,0,0,0.5)"
+          )}
+        >
+          <Flex direction="column" align="center" textAlign="center">
+            <ImQuotesLeft
+              color={useColorModeValue(
+                "${styleColors.deepBlue}",
+                "${styleColors.deepBlue}"
+              )}
+            />
             <Text fontSize={{ base: "l", md: "xl" }} fontWeight="medium" mt="6">
-              &ldquo;Clear layout & fun to use!&rdquo;
+              &ldquo;It is really simple and straight forward. Being able share
+              is also a huge plus because most (apps) restrict that so you
+              don&apos;t just arrange a ride out of the app&rdquo;
             </Text>
-            <Quotee name="Tim Ruigrok" jobTitle="Tandem User" mt="8" />
-          </Box>
-        </Flex>
-      </Box>
-      <Box
-        maxW="3xl"
-        px={{ base: "6", md: "8" }}
-        pt="12"
-        pb="16"
-        borderColor={styleColors.deepBlue}
-        borderWidth={0}
-        borderRadius={15}
-        bgColor={useColorModeValue("rgba(256,256,256,0.5)", "rgba(0,0,0,0.5)")}
-      >
-        <Flex direction="column" align="center" textAlign="center">
-          <ImQuotesLeft
-            color={useColorModeValue(
-              "${styleColors.deepBlue}",
-              "${styleColors.deepBlue}"
-            )}
-          />
-          <Box mt={41}>
-            <Text fontSize={{ base: "l", md: "xl" }} fontWeight="medium" mt="6">
-              &ldquo;Fun and fresh perspective to look at car pooling!&rdquo;
-            </Text>
-            <Quotee name="Kutay" jobTitle="Tandem User" mt="8" />
-          </Box>
-        </Flex>
-      </Box>
-      <Box
-        maxW="3xl"
-        px={{ base: "6", md: "8" }}
-        pt="12"
-        pb="16"
-        borderColor={styleColors.deepBlue}
-        borderWidth={0}
-        borderRadius={15}
-        bgColor={useColorModeValue("rgba(256,256,256,0.5)", "rgba(0,0,0,0.5)")}
-      >
-        <Flex direction="column" align="center" textAlign="center">
-          <ImQuotesLeft
-            color={useColorModeValue(
-              "${styleColors.deepBlue}",
-              "${styleColors.deepBlue}"
-            )}
-          />
-          <Box mt={47}>
-            <Text fontSize={{ base: "l", md: "xl" }} fontWeight="medium" mt="6">
-              &ldquo;This app reminded me how much money everyone from high
-              school to now owes me. 10/10&rdquo;
-            </Text>
-          </Box>
-          <Quotee name="Oscar" jobTitle="Tandem User" mt="8" />
-        </Flex>
-      </Box>
-      <Box
-        maxW="3xl"
-        px={{ base: "6", md: "8" }}
-        pt="12"
-        pb="16"
-        borderColor={styleColors.deepBlue}
-        borderWidth={0}
-        borderRadius={15}
-        bgColor={useColorModeValue("rgba(256,256,256,0.5)", "rgba(0,0,0,0.5)")}
-      >
-        <Flex direction="column" align="center" textAlign="center">
-          <ImQuotesLeft
-            color={useColorModeValue(
-              "${styleColors.deepBlue}",
-              "${styleColors.deepBlue}"
-            )}
-          />
-          <Text fontSize={{ base: "l", md: "xl" }} fontWeight="medium" mt="6">
-            &ldquo;It is really simple and straight forward. Being able share is
-            also a huge plus because most (apps) restrict that so you don&apos;t
-            just arrange a ride out of the app&rdquo;
-          </Text>
-          <Quotee name="Adrian Ruigrok" jobTitle="Tandem User" mt="8" />
-        </Flex>
-      </Box>
-    </SimpleGrid>
+            <Quotee name="Adrian Ruigrok" jobTitle="Tandem User" mt="8" />
+          </Flex>
+        </Box>
+      </SimpleGrid>
+    </Center>
   </Box>
 );
